52717acb88dd0e6493098c6240e4d86511bd0166
landenenglish <englishlanden@gmail.com>
2022-03-09 22:04:23 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 // Import data
-
+import { postings } from './postings'
 // Import components
-import './App.css';
+import './App.css'
+import Directory from './Components/Directory'
+import Gallery from './Components/Gallery'
+import Searchbar from './Components/Searchbar'
+import Sidebar from './Components/Sidebar'
 
-function App(){
+const App = () => {
   return (
     <div>
-      <h1>craigslist</h1>
-      <div className="App">
-        {/* Your content will go here! */}
+      <h1>Landen's List</h1>
+      <div className='App'>
+        <Searchbar />
+        <Directory />
+        <Sidebar />
+        <Gallery postings={postings} />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
